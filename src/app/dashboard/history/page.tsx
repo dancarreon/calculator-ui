ef8623c1ffa1e7aff92288fae3e299440ee6d3c3
Daniel Carreon <daniel.carreong@gmail.com>
2024-08-03 22:03:53 -0600

    Changes for calculator-ui

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -18,7 +18,7 @@ export interface UserRecord {
 
 export default async function Page(): Promise<any> {
 
-  const history: UserRecord[] = await getHistory() as object;
+  const history = await getHistory() as object;
 
   const page = 0;
   const rowsPerPage = 5;
